3f1c05eea6084479b804d7a06085ada09cbfbfd1
optimistic5 <github@lvl.anonaddy.com>
2024-09-15 20:16:02 +0200

    config: add some debug

--- config.h
+++ config.h
@@ -5,3 +5,10 @@
 
 #define BOOTMAGIC_LITE_ROW 5
 #define BOOTMAGIC_LITE_COLUMN 0
+
+/* VBUS detection. */
+#define USB_VBUS_PIN GP19 
+
+/* serial.c configuration (for split keyboard). */
+#undef SOFT_SERIAL_PIN
+#define SOFT_SERIAL_PIN GP1
